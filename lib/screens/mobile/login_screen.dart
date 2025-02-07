f6f1ad598b168c8b597b1c4ed1144128474cec26
ItsukiQ <raulsebastien.baldemoro@gmail.com>
2025-02-07 14:07:12 +0800

    Fixed routing based on device

---
+++ lib/screens/mobile/login_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MobileLoginScreen extends StatelessWidget {
+  const MobileLoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Mobile Login')),
+      body: const Center(child: Text('Mobile Login Screen Placeholder')),
+    );
+  }
+}
