3ec0a196a3d7fb2582feb69c8330caabbb4786e7
Jan Vincent Elleazar <152401385+jjelleazarr@users.noreply.github.com>
2025-03-18 11:34:50 +0800

    Did some code cleaning

--- lib/controllers/manage_accounts_controller.dart
+++ lib/controllers/manage_accounts_controller.dart
@@ -1,5 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart'; // For SnackBar (optional)
+import 'package:flutter/material.dart';
 
 class ManageAccountsController {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
