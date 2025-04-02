1526dc517fec21cf0f6499aa80f14d8d523d709b
jjelleazarr <elleazarjan@gmail.com>
2025-04-03 01:07:51 +0800

    Updated system files

---
+++ ios/Flutter/flutter_export_environment 2.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/jjelleazarr/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/jjelleazarr/Documents/Android Studio Projects/se2_tigersafe"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/jjelleazarr/Documents/Android Studio Projects/se2_tigersafe/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/jjelleazarr/Documents/Android Studio Projects/se2_tigersafe/.dart_tool/package_config.json"
